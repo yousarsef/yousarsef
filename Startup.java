9d24a80e822e755dcfdb0dca1cc75674d8605f77
yousarsef <83270067+yousarsef@users.noreply.github.com>
2022-03-12 19:47:58 +0100

    Add files via upload

---
+++ Startup.java
@@ -0,0 +1,12 @@
+import view.Window;
+
+/**
+ * @author Charles
+ * <p>Classe de lancememt de l'application DES</p>
+ */
+public class Startup {
+
+	public static void main(String[] arg){
+		(new Window()).setVisible(true);
+	}
+}
